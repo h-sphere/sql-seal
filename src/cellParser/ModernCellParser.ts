bb2596a4625fe2c57d15ef0a399adf2d71256c7e
Marc Satkowski <marc.satkowski@yahoo.de>
2025-04-09 19:43:05 +0200

    broaden the union type of CellParserResult by adding number

--- src/cellParser/ModernCellParser.ts
+++ src/cellParser/ModernCellParser.ts
@@ -11,7 +11,7 @@ export interface Result {
     onRunCallback?: OnRunCallback
 }
 
-export type CellParserResult = Result | string | HTMLElement
+export type CellParserResult = Result | string | HTMLElement | number
 
 export class ModernCellParser {
 
